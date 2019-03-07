bc5248dbe73abdb76ecd525c48daef6bd74218d9
Justin <Justin@192.168.1.144>
2019-03-06 22:53:24 -0600

    DataTables set up

--- src/main/resources/static/js/hardDisk.js
+++ src/main/resources/static/js/hardDisk.js
@@ -3,35 +3,8 @@
  */
 
 
-
 $( document ).ready(function() {
 
-	
-/*Fisrt method*/	
-	
-$("#hardDiskId").change(function(){
-	 
-	 
-	 var hardDiskId = $('#hardDiskId option:selected').val();
 
-	 
-	 $.ajax({
-		 
-		    type: "GET",
-		    cache: false,
-		    url: "/admin/getOneHardDisk",
-		    data: { 
-		        'hardDiskId': hardDiskId
-		    }, // Adjuntar los campos del formulario enviado.
-
-		    success: function(data)
-		     {
-		    	$("#operatingSystem").val(data.operatingSystem.nameSystem);
-		    	$("#typeSystem").val(data.operatingSystem.typeSystem);
-		     }
-	});
-	 
-	 
- });
 
 });
